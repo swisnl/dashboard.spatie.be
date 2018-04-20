b57a9f5818cb3303d074086049025c946b5e6366
bjorn <bjorn@swis.nl>
2018-04-20 13:35:30 +0200

    Move dashboard elements around to create extra room for twitter.

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,16 +5,16 @@
 @javascript(compact('pusherKey', 'pusherCluster', 'usingNodeServer'))
 
 <dashboard id="dashboard" columns="5" rows="4">
-    <twitter :initial-tweets="{{ json_encode($initialTweets) }}" position="a1:a2"></twitter>
-    <github-events position="a3:a4"></github-events>
+    <twitter :initial-tweets="{{ json_encode($initialTweets) }}" position="a1:a4"></twitter>
     <uptime position="a1:a3"></uptime>
-    <packagist position="b1"></packagist>
-    <npm position="b2"></npm>
-    <github position="b3:b4"></github>
-    <feedback position="c1"></feedback>
-    <game-of-tests position="c2:c4"></game-of-tests>
+    <github position="b1:b2"></github>
+    <packagist position="b3"></packagist>
+    <npm position="b4"></npm>
+    <github-events position="c1:c2"></github-events>
+    <game-of-tests position="c3:c4"></game-of-tests>
     <time-weather position="d1" date-format="ddd DD/MM" time-zone="Europe/Amsterdam" weather-city="Leiden"></time-weather>
-    <fussbally position="d2:d4"></fussbally>
+    <feedback position="d2"></feedback>
+    <fussbally position="d3:d4"></fussbally>
     <swis position="e1"></swis>
     <calendar position="e2:e4"></calendar>
     <internet-connection></internet-connection>
